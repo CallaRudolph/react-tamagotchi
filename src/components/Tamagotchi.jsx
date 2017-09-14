d96cc09ddbd2b6d185f3edab9102912905bd68a2
Calla Rudolph and Max Scher <calla@>
2017-09-14 11:30:46 -0700

    remove unnecessary code from Tamagotchi model

--- src/components/Tamagotchi.jsx
+++ src/components/Tamagotchi.jsx
@@ -2,17 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Food from './Food';
 
-function Tamagotchi(props) {
+class Tamagotchi extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: null,
+    }
+    this.updateClock = this.updateClock.bind(this);
+  }
 
+  updateClock() {
+    var currentTime = new Date();
 
-  return(
-    <div>
-      <h1>Hi, I'm {props.name}!</h1>
-      <p>I was born {props.timeSinceBorn} ago. Please help me enjoy a long healthy life, by keeping the below numbers as high as possible.</p>
-      <Food foodLevel={props.food}/>
-    </div>
-  )
+    var currentHrs = currentTime.getHours();
+    var currentMins = currentTime.getMinutes();
+    var currentSecs = currentTime.getSeconds();
+
+    currentMins = (currentMins < 10 ? "0" : "") + currentMins;
+    currentSecs = (currentSecs < 10 ? "0" : "") + currentSecs
+
+    var amPM = (currentHrs < 12) ? "AM" : "PM";
+
+    currentHrs = (currentHrs === 0) ? 12 : currentHrs;
+
+    var currentTimeStr = currentHrs + ":" + currentMins + ":" + currentSecs + " " + amPM;
+
+    this.setState({currentTime: currentTimeStr});
+  }
+
+  componentWillMount() {
+    setInterval(this.updateClock, 1000);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.currentTime}</h1>
+        <h2>Hi, I'm {this.props.name}!</h2>
+        <p>I was born {this.props.timeSinceBorn} ago. Please help me enjoy a long healthy life, by keeping the below numbers as high as possible.</p>
+        <Food foodLevel={this.props.food}/>
+      </div>
+    )
+  }
 }
 
 Tamagotchi.propTypes = {
--- src/models/Tamagotchi.js
+++ src/models/Tamagotchi.js
@@ -8,12 +8,14 @@ class Tamagotchi {
     this.sleep = 10;
     this.timeBorn = new Moment();
     this.timeSinceBorn = this.setTimeSinceBorn();
+    this.lifeClock = this.timeBorn.format("h:mm:ss a");
   }
 
   setTimeSinceBorn() {
     return this.timeBorn.fromNow(true);
   }
-}
 
+}
+setInterval(this.lifeClock, 1000);
 
 export default Tamagotchi;
